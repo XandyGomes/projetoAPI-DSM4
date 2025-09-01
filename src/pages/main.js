55f57035246d382ba59ea0ddd5deaa06427da264
XandyGomes <x3.computadores@hotmail.com>
2025-09-01 19:17:13 -0300

    Aula.26.08.2025

---
+++ src/pages/main.js
@@ -0,0 +1,10 @@
+import React, {Component}  from 'react';
+import { Keyboard, ActivityIndicator } from 'react-native';
+import Icon from '@expo/vector-icons/MaterialIcons';
+import api from "../services/api"
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Container, Input, Form } from '../styles'
+
+export default class Main extends Component{
+
+}
---
+++ src/styles.js
@@ -0,0 +1,41 @@
+import styled from "styled-components/native";
+import { RectButton } from "react-native-gesture-handler";
+
+//Estilos da página Main
+export const Container = styled.View`
+  flex: 1;
+  padding: 30px;
+`;
+
+export const Form = styled.View`
+  flex-direction: row;
+  padding-bottom: 20px;
+  border-bottom-width: 1px;
+  border-color: #eee;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: "#999",
+})`
+  flex: 1;
+  height: 40px;
+  background: #eee;
+  border-radius: 4px;
+  padding: 0 15px;
+  border: 1px solid #ccc;
+`;
+
+export const SubmitButton = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+  background: #3498db;
+  border-radius: 5px;
+  margin-left: 10px;
+  padding: 0 12px;
+`;
+
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 20px;
+`;
