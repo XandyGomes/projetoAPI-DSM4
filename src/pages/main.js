654000ef00c4038ba274d125c466fe8e29acbb33
XandyGomes <x3.computadores@hotmail.com>
2025-09-01 20:35:31 -0300

    Aula.01.09.2025

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,54 @@
-import React, {Component}  from 'react';
-import { Keyboard, ActivityIndicator } from 'react-native';
-import Icon from '@expo/vector-icons/MaterialIcons';
-import api from "../services/api"
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Container, Input, Form } from '../styles'
+import React, { Component } from "react";
+import { Keyboard, ActivityIndicator } from "react-native";
+import Icon from "@expo/vector-icons/MaterialIcons";
+import api from "../services/api";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText
+} from "../styles";
 
-export default class Main extends Component{
+export default class Main extends Component {
+    state = {
+        newUser: "",
+        users: [],
+        loading: false,
+    }
+
+    async componentDidMount(){
+        const users = await AsyncStorage.getItem("users")
+        if(users){
+            this.setState({users: JSON.parse(users) })
+        }
+    }
+
+    componentDidUpdate(_, prevState){
+        const { users } = this.state
+        if(prevState.users !== users){
+            AsyncStorage.setItem("users", JSON.stringify(users))
+        }
+    }
+
+    handleAddUser = async () => {
+        try{
+            const { users, newUser } = this.state
+            this.setState({loading: true})
+            const response = await api.get(`/users/${newUser}`)
+
+
+        }catch (error){
+
+        }
+    }
+
 
 }
